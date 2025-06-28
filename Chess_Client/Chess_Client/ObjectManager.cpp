2dcc6d17c638756df93cdc8c4778ca1d56af45c2
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2025-06-29 02:03:36 +0900

    ==이거 이전 커밋 전부 무시해도 됨== 적 레이어에 넣도록 수정

--- Chess_Client/Chess_Client/ObjectManager.cpp
+++ Chess_Client/Chess_Client/ObjectManager.cpp
@@ -59,7 +59,7 @@ void CObjectManager::MakeObject(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandL
 			RequestObject->SetScale(1.f, 1.f, 1.f);
 
 			// 매니저에 넣기
-			CObjectManager::GetManager()->PushObject(RequestObject);
+			CObjectManager::GetManager()->PushEnemy(RequestObject);
 		}
 		break;
 
