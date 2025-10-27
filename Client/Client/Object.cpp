49acbfd4546b095b6a4b296f240033055f24a224
LeoPark <pkj7323@naver.com>
2025-10-27 23:54:29 +0900

    주석 제거

--- Client/Client/Object.cpp
+++ Client/Client/Object.cpp
@@ -10,10 +10,6 @@ Object::Object(const std::string& name)
 {
     // 생성 시 고유 ID가 자동으로 할당됩니다.
 }
-
-// [제거] _nextInstanceId 초기화 코드를 제거합니다.
-
-// destroy 함수는 우선 그대로 둡니다.
 void Object::destroy(std::shared_ptr<Object> obj_to_destroy, float delay)
 {
     if (!obj_to_destroy || obj_to_destroy->is_destroyed()) return;
