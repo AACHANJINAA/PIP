5a38cc005d2fbd6148cd0bbc514fcfc3db8e936f
AACHANJINAA <ckswlsdl0916@naver.com>
2025-11-10 22:51:57 +0900

    밝기 조절
    추가적으로 glTF 관련 텍스쳐 매핑 내용이 업데이트될 필요가 있음
    여러 요소를 업데이트할 예정

--- Client/Client/Gltf_Shader.hlsl
+++ Client/Client/Gltf_Shader.hlsl
@@ -206,7 +206,7 @@ float4 PS_GLTF(VS_OUTPUT In) : SV_TARGET
 
     // 5. 최종 색상 조합
     // Ambient Occlusion 적용, Emissive(자체 발광) 추가
-    float3 ambient = float3(0.7, 0.7, 0.7) * albedo * ao;
+    float3 ambient = float3(0.5, 0.5, 0.5) * albedo * ao;
     float3 color = ambient + Lo + emissiveMap;
     
     // HDR to LDR, 감마 보정 등 추가적인 톤 매핑이 필요할 수 있음
