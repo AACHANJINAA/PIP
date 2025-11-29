ad333e16d4b126d7ec6921dd0c68c63383ca39a0
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2025-11-30 05:21:36 +0900

    병합 후 문제 발생해서 파일 수정 build_skybox 절대경로로 넣어주고 있어 주석처리함 상대경로로 바꾸어도 에러나서 주석처리해줌

--- Client/Client/SceneManager.cpp
+++ Client/Client/SceneManager.cpp
@@ -19,7 +19,7 @@ SceneManager::~SceneManager()
 
 void SceneManager::initialize(ID3D12Device* device, ID3D12GraphicsCommandList* command_list)
 {
-    build_skybox(device, command_list);
+    //build_skybox(device, command_list);
 
     register_scene<Chess_Scene>("ChessScene");
     //register_scene<Lobby_Scene>("LobbyScene");
