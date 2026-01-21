8ff34309f1f5fea4f7aa08a344911e465bf0ddb7
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-01-21 20:00:11 +0900

    극명한 lighting 차이를 위해 amibent 감소 시킴

--- Client/Client/SceneManager.cpp
+++ Client/Client/SceneManager.cpp
@@ -140,7 +140,7 @@ void SceneManager::build_terrain(ID3D12Device* device, ID3D12GraphicsCommandList
     // 2. ResourceManager
     terrain->load_textures_to_resource_manager(
         "Resource\\HeightMap\\HeightMap_Material.gltf",
-		"Resource\\HeightMap\\Detail_Texture.dds"
+		"Resource\\HeightMap\\Detail_Texture3.dds"
     );
 
     // 3. GPU
