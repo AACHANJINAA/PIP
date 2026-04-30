484830422c20a4e676bf2e83874e1a9fda07190b
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-04-30 16:58:39 +0900

    GLTF 메시 그림자 인스턴싱 및 렌더링 최적화
    Renderer에 GLTF 그림자 인스턴스 그룹 관리 로직을 추가하고, build_render_list에서 GLTF 메시의 그림자 인스턴싱을 분리하여 관리하도록 개선했습니다. Root Signature에 인스턴스 월드 행렬용 SRV(t12)를 추가하고, ShadowManager에서 GLTF 메시 그림자 렌더링 시 인스턴스 드로우를 수행하도록 변경했습니다. HLSL 쉐이더도 StructuredBuffer를 통해 인스턴스별 월드 변환을 적용하도록 수정했습니다.

--- Client/Client/Renderer.cpp
+++ Client/Client/Renderer.cpp
@@ -208,6 +208,7 @@ void Renderer::build_render_list(const CameraComponent* camera)
     // 1. 기존 맵 비우기
     _renderMap.clear();
     _gltfInstanceGroups.clear();
+    _gltfShadowInstanceGroups.clear();
     _shadowRenderMap.clear();
 
     const auto& allGameObjects = ObjectManager::instance()->get_all_game_objects();
@@ -277,7 +278,12 @@ void Renderer::build_render_list(const CameraComponent* camera)
             // 4. 보간된 한계값으로 판정
             if (distXZ < shadowLimit)
             {
-                _shadowRenderMap[psoName].push_back(gameObject);
+                if (psoName == "gltf") {
+                    _gltfShadowInstanceGroups[renderComp->mesh()].push_back(gameObject);
+                }
+                else {
+                    _shadowRenderMap[psoName].push_back(gameObject);
+                }
             }
         }
     }
