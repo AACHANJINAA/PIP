e76f23a88bb4939811e4aea49de613343f1ac835
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-05-09 01:30:07 +0900

    directional light 위치 수정

--- Client/Client/LightManager.cpp
+++ Client/Client/LightManager.cpp
@@ -61,9 +61,9 @@ void LightManager::initialize(ID3D12Device* device)
     Light sun;
     sun.m_bEnable = TRUE;
     sun.m_nType = DIRECTIONAL_LIGHT;
-    sun.m_cDiffuse = { 0.8f, 0.8f, 0.8f, 1.0f };
+    sun.m_cDiffuse = { 0.5f, 0.5f, 0.5f, 1.0f };
     sun.m_cSpecular = { 0.5f, 0.5f, 0.5f, 1.0f };
-    sun.m_vDirection = { 0.05f, -0.4f, 0.82f };
+    sun.m_vDirection = { 0.05f, -0.8f, 0.5f };
     add_light(std::move(sun));
 
     update();
