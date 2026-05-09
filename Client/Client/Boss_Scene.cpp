c4ff0cded0c3be09c6df3a477e5748a75c387001
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2026-05-10 01:27:26 +0900

    보스 씬 플레이어 등장을 위한 오브젝트 배치 수정 및 올바른 것으로 추출

--- Client/Client/Boss_Scene.cpp
+++ Client/Client/Boss_Scene.cpp
@@ -14,16 +14,16 @@
 void Boss_Scene::build_objects(ID3D12Device* device, ID3D12GraphicsCommandList* commandList)
 {
 
-	SceneManager::instance()->build_skybox(device, commandList, 
-		"Resource\\SkyBox\\", 
-		"space\\space_skybox.dds", 
-		"space\\space_diffuse.dds", 
-		"space\\space_specular.dds",
-		"IBL_BRDF_LUT.dds");
+	SceneManager::instance()->build_skybox(device, commandList,
+		"Resource/SkyBox/",
+		"farmland/farmland_skybox.dds",
+		"farmland/farmland_specular.dds",
+		"farmland/farmland_diffuse.txt",
+		"BRDF.dds");
 
 	// =========================필요한 메시 로드==================================
-	ResourceManager::instance()->load_mesh("Resource/Character/BruteHi/bruteHi.gltf");
-	ResourceManager::instance()->load_mesh("Resource/Character/Brute_Walk/Brute_Walk.gltf", true, "walk");
+	//ResourceManager::instance()->load_mesh("Resource/Character/BruteHi/bruteHi.gltf");
+	//ResourceManager::instance()->load_mesh("Resource/Character/Brute_Walk/Brute_Walk.gltf", true, "walk");
 	ResourceManager::instance()->load_mesh("Resource/Character/BoneGolem/BoneGolem.gltf", true);
 	ResourceManager::instance()->load_mesh("Resource/Character/BoneGolem/BoneGolemRd.gltf", true);
 	ResourceManager::instance()->load_mesh("Resource/Character/DarkKnight/SKM_DKF_Full_With_Sword.gltf", true, "idle");
@@ -31,13 +31,13 @@ void Boss_Scene::build_objects(ID3D12Device* device, ID3D12GraphicsCommandList* 
 	dynamic_pointer_cast<ReadGLTFMesh>(idle_brute_mesh)->load_animation_only("Resource/Character/Brute_Attack_animation/Brute_Attack_animation.gltf", "attack");
 	// =========================================================================
 
-	load_scene_from_file("Resource/1-BossScene/ExportedClientData.json", device, commandList);
+	load_scene_from_file("Resource/1-BossScene/Boss_Landscape_ExportedClientData.json", device, commandList);
 
 	// 카메라 생성
 	auto cameraObject = ObjectManager::instance()->create_game_object("FreeCamera");
 	cameraObject->add_component<FreeCameraScript>();
 	cameraObject->set_layer("Camera");
-	cameraObject->transform()->set_local_position(XMFLOAT3(0.0f, 500.0f, 10.0f));
+	cameraObject->transform()->set_local_position(XMFLOAT3(0.0f, 1.0f, 0.0f));
 	cameraObject->transform()->set_local_rotation(90.0f, 0.0f, 0.0f); // 약간 아래 보기
 
 	auto cameraComp = cameraObject->add_component<CameraComponent>();
