1d5ec9049d7ee3028217708a588903af9ed89e7b
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2026-05-12 20:05:47 +0900

    엘리베이터 보이지 않던 문제점 해결 및 엘리베이터 객체가 소환되지 않던 문제점 해결

--- Client/Client/Boss_Scene.h
+++ Client/Client/Boss_Scene.h
@@ -14,6 +14,8 @@ public:
     virtual void scene_process(float deltaTime) override;
     void Spawn_UI(ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
 
+    void TestMesh(ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
+
     //void Spawn_Player(ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
     //void Spawn_Test_NPCs(ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
 };
